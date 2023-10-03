57237ca0a0dfbe85820f554ed4227f2a1325d6ae
roottnt <cherhorn538@gmail.com>
2023-10-03 11:39:44 +0800

    insert

---
+++ src/main/java/com/example/book_systems/BookSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book_systems/BookSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
